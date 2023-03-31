026bb806f5243c10fd677026d8f33aea2605b44b
Amy Kaminski <amykaminski928@gmail.com>
2023-03-31 14:09:05 -0500

    Created form to addSong + integrated form into App file

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar/NavBar';
 import DisplayMusic from './components/DisplayMusic/DisplayMusic';
 import axios from 'axios';
 import SearchBar from './components/SearchBar/SearchBar';
+import CreateSong from './components/AddMusicForm/AddMusicForm';
 
 const App = () =>  {
 
@@ -20,16 +21,24 @@ const App = () =>  {
     console.log(response.data);
     setSongData(response.data)
     }
-      const handleSearchChange = (element, value) => {
-        if (element && value) {
-          const filteredData = songData.filter(
-            (item) => item[element] && item[element] === value
-            );
-            setFilteredSongData(filteredData);
-          } else {
-            setFilteredSongData([]);
-          }
-      };
+    const handleSearchChange = (element, value) => {
+      if (element && value) {
+        const filteredData = songData.filter(
+          (item) => item[element] && item[element] === value
+          );
+          setFilteredSongData(filteredData);
+        } else {
+          setFilteredSongData([]);
+        }
+    };
+
+    async function handleAddSong(newSong) {
+      const response = await axios.post('http://127.0.0.1:8000/music/', newSong);
+      if (response.status === 201) {
+        getAllSongs();
+      }
+    }
+
     return (
     <div>
       <NavBar />
@@ -37,9 +46,10 @@ const App = () =>  {
       <button onClick={getAllSongs}>Get All Songs</button>
     
       <SearchBar songData={songData} onSearchChange={handleSearchChange} />
+      <CreateSong onAddSong={handleAddSong} />
       </div>
     );
-}
+};
 
 export default App;   
 
--- src/components/AddMusicForm/AddMusicForm.jsx
+++ src/components/AddMusicForm/AddMusicForm.jsx
@@ -1,1 +1,56 @@
 // Add new music to th api (use callback funstion as a prop to handle the form submission)
+
+import React, { useState } from 'react';
+
+const CreateSong = (props) => {
+    const [title, setTitle] = useState('');
+    const [artist, setArtist] = useState('');
+    const [album, setAlbum] = useState('');
+    const [releaseDate, setReleaseDate] = useState('');
+    const [genre, setGenre] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newSong ={
+            title: (title),
+            artist: (artist),
+            album: (album),
+            release_date: (releaseDate),
+            genre: (genre)
+        };
+        props.onAddSong(newSong);
+        setTitle('');
+        setArtist('');
+        setAlbum('');
+        setReleaseDate('');
+        setGenre('');
+        
+    }
+    return ( 
+        <form onSubmit={handleSubmit} className="form-grid">
+            <div className='form-group'>
+                <label>Title</label>
+                <input type = 'text' className="form-control" value={title} onChange={(event) => setTitle(event.target.value)}/>
+            </div>
+            <div className='form-group'>
+                <label>Artist:</label>
+                <input type= 'text' className="form-control" value={artist} onChange={(event) => setArtist(event.target.value)}/>
+            </div>
+            <div className='form-group'>
+                <label>Album:</label>
+                <input type= 'text' className="form-control" value={album} onChange={(event) => setAlbum(event.target.value)}/>
+            </div>
+            <div className='form-group'>
+                <label>Release Date:</label>
+                <input type= 'text' className="form-control" value={releaseDate} onChange={(event) => setReleaseDate(event.target.value)}/>
+            </div>
+            <div className='form-group'>
+                <label>Genre:</label>
+                <input type= 'text' className="form-control" value={genre} onChange={(event) => setGenre(event.target.value)}/>
+            </div>
+            <button type= 'submit' className="btn btn-outline-primary" style={{'margin-top': '1em'}}>ADD SONG</button>
+        </form>
+     );
+};
+ 
+export default CreateSong;
